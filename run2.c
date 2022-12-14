f496dc088f5faa08ed2851daa4b22b21ae8ca6c2
ku2045 <98422696+ku2045@users.noreply.github.com>
2022-12-14 02:26:29 -0500

    second question in C

---
+++ run2.c
@@ -0,0 +1,118 @@
+/*
+ios:: app 	The Append mode. The output sent to the file is appended to it.
+ios::ate 	It opens the file for the output then moves the read and write control to file’s end.
+ios::in 	It opens the file for a read.
+ios::out 	It opens the file for a write.
+ios::trunc 	If a file exists, the file elements should be truncated prior to its opening.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/stat.h>
+
+#include <unistd.h> // getpid used in initializing rand
+
+double now() {
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+unsigned int xorbuf(int *buffer, int size) {
+	unsigned int result = 0;
+	for (int i = 0; i < size; i++) {
+		result ^= buffer[i];
+	}
+	return result;
+}
+
+char* gen_random(const int len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    char* tmp_s = (char*) malloc(len);
+    int alphanum_size = sizeof(alphanum);
+    for (int i = 0; i < len; ++i) {
+        tmp_s[i] = alphanum[rand() % (alphanum_size - 1)];
+    }
+    return tmp_s;
+}
+
+
+int main(int argc,char* argv[]) {
+	if(argc<3){
+		printf("\n<filename> <block_size>");
+		printf("\nInsufficient arguments. Check usage \n");
+		return 0;
+		// ./file_op <filename to read from > <block_size>
+	}
+
+	char* fname = argv[1]; // first argument - file to write into / read from
+	int block_size = atoi(argv[2]); // second argument - block_size
+	int block_count = 0;
+	int block_count_counter = 0;
+
+	int fd = open(fname,O_RDONLY, 0);
+	if (fd < 0)
+	{
+		printf("\nCannot open file \n");
+		return 0;
+	}
+	int num_of_char = block_size/sizeof(unsigned int);
+	unsigned int resxor = 0;
+	unsigned int  *memblock = (unsigned int  *) calloc(num_of_char, sizeof(unsigned int ));
+	int rd;
+	long int fsize = 0;
+	double start = now();
+	int threshold = 0;
+	float breakpoint = 10.0;
+
+	// while(block_count_counter < block_count){
+	while(1){
+		rd = read(fd,memblock,block_size);	
+		fsize += rd;
+		block_count_counter += 1;
+		if(rd<num_of_char) {
+			if(rd==0) break;
+			unsigned int* substr = (unsigned int *)calloc(rd,sizeof(unsigned int));
+			for(int i=0;i<rd;i++){
+				substr[i] = memblock[i];
+			}
+			resxor ^= xorbuf(substr,rd);
+			continue;
+		}
+		// printf("%s",memblock);
+		resxor ^= xorbuf(memblock,rd);
+		if(now()-start > breakpoint){
+			threshold = 1;
+			block_count = fsize / block_size;
+			break;
+		} 
+	}
+	double end = now();	
+	double perf = fsize/(end-start);
+	double perf_mb = fsize/(1024*1024*(end-start));
+
+	if(threshold == 0) {
+		block_count = fsize/block_size;
+		printf("\nFile read in less than %f seconds \n",breakpoint);
+	}
+	printf("\n[block count] %d\n", block_count);
+	printf("\n[xor] %08x\n", resxor);
+	printf("\n[size read] %ld\n", fsize);
+	printf("\n[time] %f\n", end-start);
+	printf("\n[B/s] %f\n", perf);
+	printf("\n[MiB/s] %f\n", perf_mb);
+
+	free(memblock);
+	close(fd);
+
+return 0;
+}
+
